2b2a9f9204b09a7b9dfe928a9bc75f661c31d52c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-25 08:49:40 +0900

    chapter 07 TS

---
+++ section02/src/chapter07.ts
@@ -0,0 +1,22 @@
+// void
+
+function func1(): string {
+	return 'hello'
+}
+
+function func2(): void {
+	console.log('hello');
+}
+
+let a: void;
+a = undefined;
+
+// never
+
+function func3(): never {
+	while (true) {}
+}
+
+function func4(): never {
+	throw new Error();
+}
