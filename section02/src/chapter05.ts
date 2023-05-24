34550592ec9dfac3a726d3481359629e844b27ad
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-25 08:49:29 +0900

    chapter 05 TS

--- section02/src/chapter05.ts
+++ section02/src/chapter05.ts
@@ -7,19 +7,27 @@ enum Role {
 	GUEST,
 }
 
+enum Language {
+	KOREAN = 'ko',
+	ENGLISH = 'en',
+}
+
 const user1 = {
 	name: 'jwkwon0817',
 	role: Role.ADMIN, // 0 <- Admin
+	language: Language.KOREAN,
 };
 
 const user2 = {
 	name: 'John',
 	role: Role.USER, // 1 <- User
+	language: Language.ENGLISH,
 };
 
 const user3 = {
 	name: 'Jane',
 	role: Role.GUEST, // 2 <- Guest
+	language: Language.ENGLISH,
 };
 
 console.log(user1, user2, user3);
