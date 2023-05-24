eb3203694060a36f5874748946ce7180b9285af8
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-25 02:30:56 +0900

    chapter 03 TS

---
+++ section02/src/chapter03.ts
@@ -0,0 +1,26 @@
+// object
+let user: {
+	id?: number;
+	name: string;
+} = {
+	id: 1,
+	name: 'jwkwon0817',
+};
+
+let dog: {
+	name: string;
+	color: string;
+} = {
+	name: 'Moongae',
+	color: 'white',
+};
+
+user = {
+	name: 'John',
+};
+
+let config: {
+	readonly apiKey: string;
+} = {
+	apiKey: 'MY_API_KEY',
+};
