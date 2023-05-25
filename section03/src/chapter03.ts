f26f1ac70d04752eeff68a89f9abb021fa152330
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-25 23:18:32 +0900

    chapter 03 TS

--- section03/src/chapter03.ts
+++ section03/src/chapter03.ts
@@ -1,3 +1,81 @@
 /**
- * Unknown TYpe
+ * Compatibility between Primitive Types
  */
+
+let num1: number = 10;
+let num2: 10 = 10;
+
+num1 = num2;
+
+/**
+ * Compatibility between Object Types
+ * -> Is it okay to treat some object types as other object types?
+ */
+
+type Animal = {
+	name: string;
+	color: string;
+};
+
+type Dog = {
+	name: string;
+	color: string;
+	breed: string;
+};
+
+let animal: Animal = {
+	name: 'Giraffe',
+	color: 'yellow',
+};
+
+let dog: Dog = {
+	name: 'Moongae',
+	color: 'white',
+	breed: 'Bichon Frise',
+};
+
+animal = dog;
+// dog = animal;
+
+type Book = {
+	name: string;
+	price: number;
+};
+
+type ProgrammingBook = {
+	name: string;
+	price: number;
+	skill: string;
+}
+
+let book: Book;
+
+let programmingBook: ProgrammingBook = {
+	name: 'TypeScript Programming',
+	price: 20000,
+	skill: 'TypeScript',
+};
+
+book = programmingBook;
+// programmingBook = book;
+
+/**
+ * Excess Property Check
+ */
+
+let book2: Book = {
+	name: 'TypeScript Programming',
+	price: 20000,
+	// skill: 'TypeScript',
+};
+
+let book3: Book = programmingBook;
+
+function func(book: Book) {}
+func({
+	name: 'TypeScript Programming',
+	price: 20000,
+	// skill: 'TypeScript',
+});
+
+func(programmingBook);
