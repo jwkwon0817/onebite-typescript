a6df290e259bddb4dfc3a2a899d55adf862d4237
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-26 15:42:30 +0900

    chapter 01 TS

---
+++ section04/src/chapter01.ts
@@ -0,0 +1,28 @@
+/**
+ * Function Type Expression
+ */
+
+type Operation = (a: number, b: number) => number;
+
+const add: (a: number, b: number) => number = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const mul: Operation = (a, b) => a * b;
+const div: Operation = (a, b) => a / b;
+
+/**
+ * Call Signature
+ */
+
+type Operation2 = {
+	(a: number, b: number): number;
+	name: string;
+};
+
+function func(a: number): void {}
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const mul2: Operation2 = (a, b) => a * b;
+const div2: Operation2 = (a, b) => a / b;
+
+add2.name;
