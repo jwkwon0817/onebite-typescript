3a0edc76f8cbad48c8acf3a4869d5d09b55ed229
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-28 01:03:38 +0900

    chapter 00 TS

--- section09/src/chapter00.ts
+++ section09/src/chapter00.ts
@@ -20,3 +20,22 @@ type B = ObjB extends ObjA ? number : string;
  */
 
 type StringNumberSwitch<T> = T extends number ? string : number;
+
+let varA: StringNumberSwitch<number>;
+
+let varB: StringNumberSwitch<string>;
+
+function removeSpaces<T>(text: T): T extends string ? string : undefined;
+
+function removeSpaces(text: any) {
+	if (typeof text === 'string') {
+		return text.replaceAll(' ', '');
+	} else {
+		return undefined;
+	}
+}
+
+let result = removeSpaces('Hi, I am jwkwon0817!');
+result.toUpperCase();
+
+let result2 = removeSpaces(undefined);
