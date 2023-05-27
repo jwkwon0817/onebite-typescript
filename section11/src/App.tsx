5a56afec473fb74e3ea50765e3e03c44d26bcc55
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-28 02:23:17 +0900

    App TSX

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useReducer, useRef } from 'react';
+import React, { useContext, useEffect, useReducer, useRef } from 'react';
 import Editor from './components/Editor';
 import TodoItem from './components/TodoItem';
 import { Todo } from './types';
@@ -24,6 +24,22 @@ function reducer(state: Todo[], action: Action) {
 		case 'DELETE':
 			return state.filter((todo) => todo.id !== action.data.id);
 	}
+}
+
+export const TodoStateContext = React.createContext<Todo[] | null>(null);
+export const TodoDispatchContext = React.createContext<{
+	onClickAdd: (text: string) => void,
+	onClickDelete: (id: number) => void,
+} | null>(null);
+
+export function useTodoDispatch() {
+	const dispatch = useContext(TodoDispatchContext);
+	
+	if (!dispatch) {
+		throw new Error('An error occurred while dispatching the action.');
+	}
+	
+	return dispatch;
 }
 
 function App() {
@@ -59,18 +75,24 @@ function App() {
 	return (
 		<div className={ 'App' }>
 			<h1>Todo</h1>
-			<Editor onClickAdd={ onClickAdd }/>
-			<div>
-				{
-					todos.map((todo) => {
-						return (
-							<div key={ todo.id }>
-								<TodoItem key={ todo.id } { ...todo } onClickDelete={ onClickDelete }/>
-							</div>
-						);
-					})
-				}
-			</div>
+			<TodoStateContext.Provider value={ todos }>
+				<TodoDispatchContext.Provider value={ {
+					onClickAdd, onClickDelete,
+				} }>
+					<Editor/>
+					<div>
+						{
+							todos.map((todo) => {
+								return (
+									<div key={ todo.id }>
+										<TodoItem key={ todo.id } { ...todo }/>
+									</div>
+								);
+							})
+						}
+					</div>
+				</TodoDispatchContext.Provider>
+			</TodoStateContext.Provider>
 		</div>
 	);
 }
