5e2426f9c48bc9f455696d0a7fb2014695a635ea
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-28 01:03:42 +0900

    chapter 01 TS

---
+++ section09/src/chapter01.ts
@@ -0,0 +1,25 @@
+/**
+ * Decentralized Conditional Types
+ */
+
+type StringNumberSwitch<T> = [ T ] extends [ number ] ? string : number;
+
+let a: StringNumberSwitch<number>;
+
+let b: StringNumberSwitch<string>;
+
+let c: StringNumberSwitch<number | string>;
+
+let d: StringNumberSwitch<boolean | number | string>;
+
+/**
+ * Useful Conditional Types
+ */
+
+type Exclude<T, U> = T extends U ? never : T;
+
+type A = Exclude<number | string | boolean, string>;
+
+type Extract<T, U> = T extends U ? T : never;
+
+type B = Extract<number | string | boolean, string>;
